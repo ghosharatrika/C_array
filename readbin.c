62112877dec91c209f4110aa2d17787e6d9989af
ghosharatrika <150947458+ghosharatrika@users.noreply.github.com>
2023-11-16 20:25:52 +0530

    Add files via upload

---
+++ readbin.c
@@ -0,0 +1,25 @@
+// This program reads the content of result2.bin and display it on the terminal
+
+#include <stdio.h>
+
+int main() {
+    // Open the binary file for reading
+    FILE *file = fopen("result2.bin", "rb");
+    if (file == NULL) {
+        fprintf(stderr, "Error opening file for reading.\n");
+        return 1;
+    }
+
+    // Read the result from the binary file
+    float result[2];
+    fread(result, sizeof(float), 2, file);
+
+    // Close the file
+    fclose(file);
+
+    // Display the result
+    printf("Mean: %.2f\n", result[0]);
+    printf("Variance: %.2f\n", result[1]);
+
+    return 0;
+}
