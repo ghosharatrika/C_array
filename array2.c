7dcd6941934414ff1ce6de79b22edee76c0c4281
ghosharatrika <150947458+ghosharatrika@users.noreply.github.com>
2023-11-16 20:00:29 +0530

    Create array2.c

---
+++ array2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "statistics.h"
+
+
+int main() {
+    
+    float *array = (float *)malloc(100 * sizeof(float)); // Using malloc, create an array of hundred float elements
+
+    for (int i = 0; i < 100; ++i) {
+        array[i] = (i+1)*(i+1); // Assign values to the elements of the array
+    }
+   
+    float *result = calculate_mean_variance(array, 100); // Calculate mean and variance using the function
+
+    printf("Mean: %f\nVariance: %f\n", result[0], result[1]); // Print the results
+
+    // Free the memory used for the array
+    free(array); 
+    free(result);
+
+    return 0;
+}
