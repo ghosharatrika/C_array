2dd25f6cfb5d75d142aad3f224f09e064d54c3c9
ghosharatrika <150947458+ghosharatrika@users.noreply.github.com>
2023-11-17 10:48:31 +0530

    Update array_gsl.c

--- array_gsl.c
+++ array_gsl.c
@@ -8,19 +8,19 @@
 
 int main() {
     
-    float *array = (float *)calloc(100 ,sizeof(float)); // Using calloc, create an array of hundred float elements
+    double *array = (double *)calloc(100 ,sizeof(double)); // Using calloc, create an array of hundred float elements
 
     for (int i = 0; i < 100; ++i) {
         printf("Assigning value to array[%d]\n", i);
         array[i] = (i+1)*(i+1); // Assign values to the elements of the array
     }
    
-    float *result = calculate_mean_variance(array, 100); // Calculate mean and variance using the function
+    double *result = calculate_mean_variance(array, 100); // Calculate mean and variance using the function
     printf("Mean: %f\nVariance: %f\n", result[0], result[1]); // Print the results using loops
 
     // Calculate mean and variance using GSL functions
-    double gsl_mean = gsl_stats_mean(arr, 1, 100);
-    double gsl_variance = gsl_stats_variance(arr, 1, 100);
+    double gsl_mean = gsl_stats_mean(array, 1, 100);
+    double gsl_variance = gsl_stats_variance(array, 1, 100);
 
     // Print mean and variance using GSL functions
     printf("\nGSL Mean: %.2f\n", gsl_mean);
