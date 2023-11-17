1d695c03273a90a7d36cc81bd4bb869c2748d6f4
ghosharatrika <150947458+ghosharatrika@users.noreply.github.com>
2023-11-17 10:45:49 +0530

    Update array1.c

--- array1.c
+++ array1.c
@@ -6,13 +6,13 @@
 
 int main() {
     
-    float *array = (float *)malloc(100 * sizeof(float)); // Using malloc, create an array of hundred float elements
+    double *array = (double *)malloc(100 * sizeof(double)); // Using malloc, create an array of hundred float elements
 
     for (int i = 0; i < 100; ++i) {
         array[i] = (i+1)*(i+1); // Assign values to the elements of the array
     }
    
-    float *result = calculate_mean_variance(array, 100); // Calculate mean and variance using the function
+    double *result = calculate_mean_variance(array, 100); // Calculate mean and variance using the function
 
     printf("Mean: %f\nVariance: %f\n", result[0], result[1]); // Print the results
 
