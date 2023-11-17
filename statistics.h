6eb3b02b23f58139cc7723391eae20ff4bbced95
ghosharatrika <150947458+ghosharatrika@users.noreply.github.com>
2023-11-17 11:40:10 +0530

    Update statistics.h

--- statistics.h
+++ statistics.h
@@ -1,6 +1,6 @@
 #ifndef STATISTICS_H
 #define STATISTICS_H
 
-float *calculate_mean_variance(float *array, int length);
+double *calculate_mean_variance(double *array, int length);
 
 #endif // STATISTICS_H
