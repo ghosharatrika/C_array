caaf987830fa92584780984c2fa6f63d31443177
ghosharatrika <150947458+ghosharatrika@users.noreply.github.com>
2023-11-17 10:50:23 +0530

    Update calloc_array.c

--- calloc_array.c
+++ calloc_array.c
@@ -5,14 +5,14 @@
 
 int main() {
     
-    float *array = (float *)calloc(100 ,sizeof(float)); // Using calloc, create an array of hundred float elements
+    double *array = (double *)calloc(100 ,sizeof(double)); // Using calloc, create an array of hundred float elements
 
     for (int i = 0; i < 100; ++i) {
         printf("Assigning value to array[%d]\n", i);
         array[i] = (i+1)*(i+1); // Assign values to the elements of the array
     }
    
-    float *result = calculate_mean_variance(array, 100); // Calculate mean and variance using the function
+    double *result = calculate_mean_variance(array, 100); // Calculate mean and variance using the function
 
     printf("Mean: %f\nVariance: %f\n", result[0], result[1]); // Print the results
 
